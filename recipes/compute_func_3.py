207f952978bae9f2d231362a80572d850067d776
pierrepetrella <pierrepetrella>
2024-10-09 14:55:15 +0100

    Saved recipe: MASTER_PROJECT_DEV2_B.compute_func_3

--- recipes/compute_func_3.py
+++ recipes/compute_func_3.py
@@ -3,18 +3,17 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
+from transformation_functions import func_3
+
 # Read recipe inputs
 medium_articles = dataiku.Dataset("medium_articles")
-medium_articles_df = medium_articles.get_dataframe()
+df = medium_articles.get_dataframe()
 
-
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
-func_3_df = medium_articles_df # For this sample code, simply copy input to output
+columns = ["link"]
+func_3_df = func_3(df,columns) 
 
 
 # Write recipe outputs
 func_3 = dataiku.Dataset("func_3")
 func_3.write_with_schema(func_3_df)
+
